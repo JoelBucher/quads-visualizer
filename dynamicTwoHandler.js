72533b57c5c413f4f9c211ef0ba5836d1d31d53a
JoelBucher <JoelBucher@users.noreply.github.com>
2023-01-09 09:03:32 +0000

    deploy: 280567e8f0e03c28e877de2785620b448c263e86

--- dynamicTwoHandler.js
+++
@@ -1,8 +0,0 @@
-//import {dynamicTwo } from '../algorithms/dynamicTwo.js';
-
-function dynamicTwoHandler () {
-    self.addEventListener('message', e => { // eslint-disable-line no-restricted-globals
-        let seconds = e.data;
-        postMessage(seconds + 6)
-    })
-}
